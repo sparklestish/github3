68952980bbe7c96f55c4e6435aca61c3e7752064
sparklestish <richardneaverscoy@gmail.com>
2019-01-22 17:44:32 +0000

    new
    head

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={	
+	'ApiUrl':"http://142.4.5.189/api",						
+	'DialogDefaultTitle':"Agent247",    
+	'APIHasKey':"myapikeys",
+	'debug': false
+};
